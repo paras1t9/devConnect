76c491d9c943a36b21d646e625f900ec2904d361
ayush <ayush.mehta010906@gmail.com>
2026-06-29 16:40:33 +0000

    directory changes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Input from "../components/Input"
-import Button from "../components/Button";
+import Input from "../components/common/Input"
+import Button from "../components/common/Button";
 import { registerUser } from "../services/api"
 
 function Register() {

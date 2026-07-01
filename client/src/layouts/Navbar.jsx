ab98ea1e4b99b4584ca1db4cc4477f7d26b0e126
ayush <ayush.mehta010906@gmail.com>
2026-07-01 13:27:37 +0000

    navbar cient

--- client/src/layouts/Navbar.jsx
+++ client/src/layouts/Navbar.jsx
@@ -1,21 +1,27 @@
+import { Menu } from "lucide-react";
 import { useAuth } from "../context/AuthContext";
-import { Link } from "react-router-dom";
 
 export default function Navbar({ toggleSidebar }) {
   const { user } = useAuth();
 
   return (
-    <header>
-      <div>
-        <button onClick={toggleSidebar}>☰</button>
-        <Link to={"/dashboard"}>
-          <h1>DevConnect</h1>
-        </Link>
+    <header className="flex items-center justify-between h-16 px-6 bg-white border-b shadow-sm">
+      <div className="flex items-center gap-4">
+        <button onClick={toggleSidebar}>
+          <Menu size={24} />
+        </button>
+
+        <h1 className="text-xl font-bold">
+          DevConnect
+        </h1>
       </div>
 
-      <div>
+      <div className="flex items-center gap-4">
         <span>{user?.name}</span>
-        <button>Logout</button>
+
+        <button className="text-red-600 font-medium">
+          Logout
+        </button>
       </div>
     </header>
   );
